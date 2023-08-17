c81289d103c1109f2821c4f81e3836e7dff95b92
Harshkate2124 <106866593+Harshkate2124@users.noreply.github.com>
2023-08-17 22:21:22 +0530

    Uploaded all files except .py file

---
+++ pages/About Me.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+
+
+
+st.text("Hello Jarvis I'm Harshal Kate , more coder less debugger ;")
+st.text("I'm passionate about Data Science and Software Developer ")
+st.text("")
